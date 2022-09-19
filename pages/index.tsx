0e71867d7bd1d369de6f1e40188882d7af5f6784
aszarth <aszarth@gmail.com>
2022-09-19 09:40:43 -0300

    start to use carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,21 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
-import Title from "../components/Title";
+import { Carousel } from "react-responsive-carousel";
 
 export default function Home() {
   return (
     <div>
-      <div>
-        <span>Home</span>
-      </div>
-      <div
-        style={{
-          marginLeft: 80,
-          marginRight: 80,
-          marginTop: 10,
-        }}>
-        <Carousel>
-          <div>
-            <img src="/amanda_alves.jpg" />
-          </div>
-          <div>
-            <img src="/mylaine_ivo.png" />
-          </div>
-          <div>
-            <img src="/tmplogo.png" />
-          </div>
-        </Carousel>
-      </div>
+      <Carousel showThumbs={false} showArrows={false} dynamicHeight={false}>
+        <div>
+          <img src="/c1.webp" height={510} />
+        </div>
+        <div>
+          <img src="/c2.webp" height={510} />
+        </div>
+        <div>
+          <img src="/c3.webp" height={510} />
+        </div>
+      </Carousel>
     </div>
-
-
-  )
+  );
 }
